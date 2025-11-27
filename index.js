2f0c2c3f1eb47cb02615c9dcfdc07e8382f99262
Miquel Piña Grau <miquel.mpg@gmail.com>
2025-11-28 00:05:26 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((totalSum, number) => {
+      return totalSum + number;
+    }, 0)
+  }
 
-  avg() {}
+  avg() {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items.reduce((totalAverage, number, index, array) =>{
+      return totalAverage + (number / array.length);
+    }, 0)
+  }
 }
 
 module.exports = SortedList;
